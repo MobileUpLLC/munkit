a8d9c6a6811796277eb1558e2eca2f1c1329199b
Natalia Luzyanina <natalia.luzyanina@mobileup.ru>
2025-03-20 16:24:48 +0300

    [UPUP-1002] Правки

--- Sources/NetworkService/Replica/Common/AbstractLoadable.swift
+++ Sources/NetworkService/Replica/Common/AbstractLoadable.swift
@@ -1,12 +1,12 @@
 /// Определяет базовое состояние загрузки данных для объекта.
 public protocol AbstractLoadable {
-    associatedtype Data
+    associatedtype T
 
     /// Указывает, выполняется ли загрузка в данный момент.
-    var loading: Bool { get }
+    var isLoading: Bool { get }
 
     /// Данные, если они доступны, или nil, если загрузка ещё не завершена или произошла ошибка.
-    var data: Data? { get }
+    var data: T? { get }
 
     /// Ошибка загрузки, если она произошла, или nil, если ошибок нет.
     var error: CombinedLoadingError? { get }
