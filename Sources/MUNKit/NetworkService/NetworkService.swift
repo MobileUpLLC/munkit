d5c9fb2add4455172fb923591211ea50e2d9c3d8
Ilia Chub <ilia_chub@icloud.com>
2025-04-20 02:59:01 +0300

    Fix multiple token refresh error

--- Sources/MUNKit/NetworkService/NetworkService.swift
+++ Sources/MUNKit/NetworkService/NetworkService.swift
@@ -25,32 +25,70 @@ public actor MUNNetworkService<Target: MUNAPITarget> {
         tokenRefreshFailureHandler = action
     }
 
+    private var activeRequests: Set<UUID> = []
+    private var requestsPendingTokenRefresh: Set<UUID> = []
+
     public func executeRequest<T: Decodable & Sendable>(
         target: Target,
         isTokenRefreshed: Bool = false
     ) async throws -> T {
-        switch await performRequest(target: target) {
+        let requestId = UUID()
+        activeRequests.insert(requestId)
+        let result = await performRequest(target: target)
+
+        switch result {
         case .success(let response):
             let filteredResponse = try response.filterSuccessfulStatusCodes()
             let result = try filteredResponse.map(T.self)
+            activeRequests.remove(requestId)
+            requestsPendingTokenRefresh.remove(requestId)
             return result
         case .failure(let error):
-            try await resolveRequestError(error, target: target, isTokenRefreshed: isTokenRefreshed)
+            do {
+                try await resolveRequestError(
+                    error,
+                    requestId: requestId,
+                    target: target,
+                    isTokenRefreshed: isTokenRefreshed
+                )
+            } catch {
+                activeRequests.remove(requestId)
+                requestsPendingTokenRefresh.remove(requestId)
+            }
             return try await executeRequest(target: target, isTokenRefreshed: true)
         }
     }
 
     public func executeRequest(target: Target, isTokenRefreshed: Bool = false) async throws {
+        let requestId = UUID()
+        activeRequests.insert(requestId)
         switch await performRequest(target: target) {
         case .success(let response):
             let _ = try response.filterSuccessfulStatusCodes()
+            activeRequests.remove(requestId)
+            requestsPendingTokenRefresh.remove(requestId)
         case .failure(let error):
-            try await resolveRequestError(error, target: target, isTokenRefreshed: isTokenRefreshed)
+            do {
+                try await resolveRequestError(
+                    error,
+                    requestId: requestId,
+                    target: target,
+                    isTokenRefreshed: isTokenRefreshed
+                )
+            } catch {
+                activeRequests.remove(requestId)
+                requestsPendingTokenRefresh.remove(requestId)
+            }
             try await executeRequest(target: target, isTokenRefreshed: true)
         }
     }
 
-    private func resolveRequestError(_ error: MoyaError, target: Target, isTokenRefreshed: Bool) async throws {
+    private func resolveRequestError(
+        _ error: MoyaError,
+        requestId: UUID,
+        target: Target,
+        isTokenRefreshed: Bool
+    ) async throws {
         guard
             target.isAccessTokenRequired,
             isTokenRefreshed == false,
@@ -65,6 +103,11 @@ public actor MUNNetworkService<Target: MUNAPITarget> {
             return try await tokenRefreshTask.value
         }
 
+        if requestsPendingTokenRefresh.contains(requestId) {
+            return
+        }
+
+        requestsPendingTokenRefresh = activeRequests
         tokenRefreshTask = _Concurrency.Task { [weak self] in
             try await self?.tokenProvider.refreshToken()
         }
