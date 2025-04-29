aba0a6f7997c88b82b928e5bade1a7edf38dbc2f
Ilia Chub <ilia_chub@icloud.com>
2025-04-29 20:12:20 +0300

    Remake the iOS example project

---
+++ examples/munkit-example-ios/munkit-example-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  munkit-example-ios
+//
+//  Created by Ilia Chub on 29.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ examples/munkit-example-ios/munkit-example-ios/munkit_example_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  munkit_example_iosApp.swift
+//  munkit-example-ios
+//
+//  Created by Ilia Chub on 29.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct munkit_example_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
