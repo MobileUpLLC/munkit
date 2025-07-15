98a80577ab001044241f7d2f9e5fcc9d366cfc49
Ilia Chub <ilia_chub@icloud.com>
2025-07-15 16:32:13 +0300

    Commit to remove

--- examples/munkit-example-core/Sources/munkit-example-core/models/monsters/DNDMonsterShortModel.swift
+++ examples/munkit-example-core/Sources/munkit-example-core/models/monsters/DNDMonsterShortModel.swift
@@ -5,6 +5,6 @@
 //  Created by Ilia Chub on 12.05.2025.
 //
 
-public struct DNDMonsterShortModel: Sendable, Decodable {
+public struct DNDMonsterShortModel: Sendable, Decodable, Hashable {
     public let index, name, url: String
 }
